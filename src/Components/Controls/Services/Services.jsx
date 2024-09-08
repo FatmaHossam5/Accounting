7908d8df86fb2ceb9a7090163d81c0e52c1b34fe
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2024-09-08 17:58:48 +0300

    serviceModal

--- src/Components/Controls/Services/Services.jsx
+++ src/Components/Controls/Services/Services.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import Dropdown from '../../Shared/Dropdown/Dropdown';
 import CustomPage from '../../Shared/CustomPage/CustomPage';
+import Modal from '../../Shared/Modal/Modal';
+import Input from '../../Shared/Input/Input';
+import Select from '../../Shared/Select/Select';
 
 export default function Services() {
   const [openDropdownId, setopenDropdownId] = useState(null)
   const columns = [
     {
-      name: " Name",
+      name: "Service Name",
       selector: (row) => { row.name },
       sortable: true,
       cell: (row) => (
-        <div className="d-flex align-items-center ">
-          <span className="me-2">
+        <div className="d-flex align-items-center justify-content-between" style={{ minWidth: '150px' }}>
+          <span className="text-truncate" style={{ maxWidth: '150px' }}>
             {row.name}
           </span>
           <Dropdown
@@ -33,7 +36,7 @@ export default function Services() {
             openDropdownId={openDropdownId}
             setOpenDropdownId={setopenDropdownId}
           />
-        
+
         </div>
       ),
 
@@ -43,141 +46,152 @@ export default function Services() {
 
     },
     {
-      name: "BRAND",
-      selector: (row) => row.brand,
-      sortable: true,
-    },
-    {
-      name: "TAX",
-      selector: (row) => row.tax,
-      sortable: true,
-    },
-    {
-      name: "Taxable",
-      selector: (row) => row.Taxable,
-      sortable: true,
-    },
-    {
-      name: "Description",
+      name: "Service Description",
       selector: (row) => row.description,
       sortable: true,
     },
     {
-      name: "STOCK",
-      selector: (row) => row.stock,
+      name: "Service Department",
+      selector: (row) => row.department,
       sortable: true,
     },
     {
-      name: "Daily Income",
-      selector: (row) => row.daily,
+      name: "Service Branch",
+      selector: (row) => (<div className='  ms-5'>
+        {row.branch}
+      </div>),
       sortable: true,
     },
     {
-      name: "Weekly Income",
-      selector: (row) => row.weekly,
+      name: "Service Type",
+      selector: (row) => row.type,
       sortable: true,
     },
-    {
-      name: "Monthly Income",
-      selector: (row) => row.monthly,
-      sortable: true,
-    },
-    {
-      name: "Yearly Income",
-      selector: (row) => row.yearly,
-      sortable: true,
-    },
-    {
-      name: "App Category",
-      selector: (row) => row.category,
-      sortable: true,
-    },
-    {
-      name: "Contact Email",
-      selector: (row) => row.email,
-      sortable: true,
-    },
-    {
-      name: "Address",
-      selector: (row) => row.address,
-      sortable: true,
-    },
-    {
-      name: "Postal Code",
-      selector: (row) => row.postal,
-      sortable: true,
-    },
+
   ];
 
   const data = [
     {
       id: 1,
-      name:'pixi1',
-      brand: "PIXI",
-      tax: "25",
-      Taxable: "YES",
-      description: "Good",
-      stock: "yes",
-      daily: "1500",
-      weekly: '2500',
-      monthly: '5000',
-      yearly: '1000000',
-      category: '1',
-      email: "tomafhf@gmail.com",
-      address: 'Nasr',
-      postal: '154759',
-      
+      name: 'Marketing Facebook Ads',
+      description: "This service is for making ads ...",
+      department: "Marketing",
+      branch: "All branches",
+      type: 'Performance Marketing',
+
+
     },
     {
       id: 2,
-      name:'pixi2',
-      brand: "PIXI",
-      tax: "25",
-      Taxable: "YES",
-      description: "Good",
-      stock: "yes",
-      daily: "1500",
-      weekly: '2500',
-      monthly: '5000',
-      yearly: '1000000',
-      category: '1',
-      email: "tomafhf@gmail.com",
-      address: 'Nasr',
-      postal: '154759',
-      
+      name: 'Email Marketing Campaigns',
+      description: "Designing and executing tar ...",
+      department: "Public Relations",
+      branch: "All branches",
+      type: 'Direct Marketing',
+
+
     },
 
     {
       id: 3,
-      name:'pixi3',
-      brand: "PIXI",
-      tax: "25",
-      Taxable: "YES",
-      description: "Good",
-      stock: "yes",
-      daily: "1500",
-      weekly: '2500',
-      monthly: '5000',
-      yearly: '1000000',
-      category: '1',
-      email: "tomafhf@gmail.com",
-      address: 'Nasr',
-      postal: '154759',
-      
+      name: 'Influencer Marketing',
+      description: "Collaborating with influencers...",
+      department: "Sales and Business Development",
+      branch: "All branches",
+      type: 'Referral Marketing',
+
+
     },
   ];
   return (
     <>
+
+      <Modal id='createService' className={'w-50'} title={'createNewService'}>
+        <div className='d-flex justify-content-center '>
+          <div className='me-2'>
+            <Input label={'Service English Name'} placeholder={'Enter service name'} />
+          </div>
+          <div>
+            <Input label={'Service Arabic Name'} placeholder={'Enter service name'} />
+          </div>
+
+        </div>
+        <div className='col-10  m-auto mt-3'>
+          <Input placeholder={'Enter description'} label={'Service Description'} />
+        </div>
+        <div className='d-flex justify-content-center col-10 mt-3 m-auto g-5'>
+          <div className='me-4'>
+            <Select
+              label={'Service Department'}
+
+              options={[
+                { value: '', label: 'Select Department' },
+                { value: 'Marketing', label: 'Marketing' },
+                { value: 'Sales and Business Development', label: 'Sales' },
+                { value: 'Public Relations', label: 'Public Relations' },
+                { value: 'Content Creation', label: 'Content Creation' },
+                { value: 'Advertising', label: 'Advertising' },
+
+              ]}
+            />
+          </div>
+          <div className='me-4'>
+
+            <Select
+              label={'Service Branch'}
+
+              options={[
+                { value: '', label: 'All Branches' },
+                { value: 'Marketing', label: 'Marketing' },
+                { value: 'Sales and Business Development', label: 'Sales' },
+                { value: 'Public Relations', label: 'Public Relations' },
+                { value: 'Content Creation', label: 'Content Creation' },
+                { value: 'Advertising', label: 'Advertising' },
+
+              ]}
+            />
+          </div>
+          <div className=''>
+
+            <Select
+              label={'Service Type'}
+
+              options={[
+                { value: '', label: 'Select Type' },
+                { value: 'Marketing', label: 'Marketing' },
+                { value: 'Sales and Business Development', label: 'Sales' },
+                { value: 'Public Relations', label: 'Public Relations' },
+                { value: 'Content Creation', label: 'Content Creation' },
+                { value: 'Advertising', label: 'Advertising' },
+
+              ]}
+            />
+          </div>
+        </div>
+        <div className='d-flex justify-content-center mt-3 '>
+          <div className='me-2 '>
+            <Input label={'Unit Price'} placeholder={'Enter unit price'} />
+          </div>
+          <div>
+            <Input label={'Service Tax'} placeholder={'Enter service tax'} />
+          </div>
+
+        </div>
+        {/* <div className="modal-footer w-100">
+          <button type="button" className="px-btn btn px-white-btn" data-bs-dismiss="modal">Cancel</button>
+          <button type="button" className="px-btn px-blue-btn">save</button>
+        </div> */}
+      </Modal>
       <CustomPage data={data}
         columns={columns}
         title='Services'
         ButtonName='CreateService'
         ModalTitle='CreateService'
         target='#createService'
-        targetId='createService'
+
       />
-    
-    
+
+
     </>
   )
 }
--- src/Components/Shared/AddInvoice/AddInvoice.jsx
+++ src/Components/Shared/AddInvoice/AddInvoice.jsx
@@ -2,190 +2,231 @@ import React, { useState } from 'react'
 
 
 export default function AddInvoice({pageTitle,subTitle,labelName,htmlFor,optionName,InvoiceType}) {
- const[showItem,setShowItem]=useState([{id:1}]);
- 
- 
- const handleAddItem=()=>{
 
-  
-  setShowItem([...showItem,{id:showItem.length+1}])
-  console.log(showItem);
- }
+const [items, setItems] = useState([{ item: '', price: 0, count: 0 }]);
+const [total, setTotal] = useState(0);
 
- const handleRemoveItem=(id)=>{
-  setShowItem(showItem.filter(item=>item.id!==id))
- }
+const handleAddItem = () => {
+  setItems([...items, { item: '', price: 0, count: 0 }]);
+};
+
+const handleRemoveItem = (id) => {
+setItems(items.filter((item)=>item.id!== id))
+};
+
+const handleItemChange = (index, field, value) => {
+  const newItems = items.map((item, i) => 
+    i === index ? { ...item, [field]: value } : item
+  );
+  setItems(newItems);
+  calculateTotal(newItems);
+};
+
+const calculateTotal = (items) => {
+  const total = items.reduce((acc, item) => acc + (item.price * item.count), 0);
+  setTotal(total);
+};
+
   return (
-    <><div className="px-content mb-auto mt-3">
-  <div className="px-card ">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 gx-0">
-          <div className="d-flex p-4 flex-wrap">
-            <div className="breadcrumbs-list w-100 mb-3">
-              <ul className="d-flex">
-                <li className="breadcrumbs-item d-flex align-items-center">
-               {InvoiceType}
-                </li>
-                <li className="breadcrumbs-item d-flex align-items-center active-breadcrumbs">
-                  <span className="breadcrumbs-separetor ms-2"><i className="fa-kit fa-right" /></span>{`Create ${InvoiceType}`}
-                </li>
-              </ul>
-            </div>
-            <div className="page-name  ">
-              <h3>{pageTitle}</h3>
-            </div>
-          </div>
-          {/* start create customer invoice form */}
-          {/* Modal */}
-          <form action className="d-flex flex-wrap ">
-            <div className="side w-xxl-100 w-100 p-4 ">
-              <div className="section">
-                <h4>{subTitle}</h4>
-                <div className="form-inputs d-flex w-100">
-                  <div className="input-package mt-3 pe-2 d-flex flex-column w-50">
-                    
-                    <label className="mb-2" htmlFor={htmlFor}>{labelName}</label>
-                    <select name="customer" id="customer" className="px-login-input  w-100 m-auto">
-                      <option value>{optionName}</option>
-                      <option value="ahmed">ahmed</option>
-                      <option value="johan">johan</option>
-                      <option value="samy">samy</option>
-                    </select>
-                  </div>
-                  <div className="input-package mt-3 ps-2 d-flex flex-column w-50">
-                    <label className="mb-2" htmlFor="cost-center">cost
-                      center</label>
-                    <select name="cost-center" id="cost-center" className="px-login-input w-100 m-auto">
-                      <option value>General cost center</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="input-package mt-3  d-flex flex-column w-100">
-                  <label htmlFor="companyIndustry">Supplier Address</label>
-                  <textarea name="address" id="address" className="px-text-area" placeholder="Enter supplier address" defaultValue={""} />
-                </div>
+    <div className="px-content mb-auto mt-3">
+    <div className="px-card">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 gx-0">
+            <div className="d-flex p-4 flex-wrap">
+              <div className="breadcrumbs-list w-100 mb-3">
+                <ul className="d-flex">
+                  <li className="breadcrumbs-item d-flex align-items-center">
+                    {InvoiceType}
+                  </li>
+                  <li className="breadcrumbs-item d-flex align-items-center active-breadcrumbs">
+                    <span className="breadcrumbs-separetor ms-2">
+                      <i className="fa-kit fa-right" />
+                    </span>
+                    {`Create ${InvoiceType}`}
+                  </li>
+                </ul>
               </div>
-              <div className="separetor my-4" />
-              <div className="section ">
-                <div className=" d-flex  mt-4">
-                  <h4>Supply Items Details</h4>
-                  <button type="button" className="px-btn px-blue-btn add-item-btn ms-auto" onClick={handleAddItem}>Add Item</button>
-                </div>
-              
-                  <>
-                  <div>
-                  <div className="form-inputs  w-100 d-flex justify-content-between flex-wrap item-details-container">
-                  <div className="input-package mt-4 d-flex flex-column w-30 ">
-                    <label className htmlFor>item</label>
-                    <div className="px-calender mt-2 w-100">
-                      <select name="item" id="item" className="px-login-input w-100">
-                        <option value="item1">item1</option>
-                        <option value="item2">item2</option>
-                        <option value="item3">item3</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="input-package mt-4 d-flex flex-column w-30 ">
-                    <label className htmlFor>price</label>
-                    <div className="px-calender mt-2 w-100">
-                      <select name="item-price" id="item-price" className="px-login-input  w-100">
-                        <option value="100$">100$</option>
-                        <option value="200$">200$</option>
-                        <option value="300$">300$</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="input-package mt-4 d-flex flex-column w-30 ">
-                    <label className htmlFor>item count</label>
-                    <div className="px-calender w-100">
-                      <input type="text" className="px-form-input mt-2 w-100" placeholder={8} />
-                    </div>
-                  </div>
-                  <button type='button' className='px-btn px-red-btn remove-item-btn ms-auto' onClick={()=>  handleRemoveItem(item.id)}>-</button>
-                </div>
-                  </div>
-                  </>  
-            
-    
+              <div className="page-name">
+                <h3>{pageTitle}</h3>
+              </div>
+            </div>
 
-     
-<div className='px-dashed-card total-items-cost justify-content-between p-4 mt-4'>
-                  <div className='total-text'>
-                    <h5>total invoice</h5>
-                  </div>
-                  <div className='total-number'>
-                    <h5>2150 $</h5>
-                  </div>
-                </div>  
-                <div className="px-dashed-card total-items-cost d-none justify-content-between p-4 mt-4">
-                  <div className="total-text">
-                    <h5>total invoice</h5>
-                  </div>
-                  <div className="total-number">
-                    <h5>2150 $</h5>
-                  </div>
-                </div>
-              </div>
-              
-           
-              <div className="separetor my-4" />
-              <div className="section">
-                <h4>invoice Details</h4>
-                <div className="form-inputs d-flex flex-wrap w-100">
-                  <div className="input-package mt-4 pe-2 d-flex flex-column w-50 ">
-                    <label className htmlFor>Invoice due date</label>
-                    <div className="px-calender w-100">
-                      <input className="px-form-input w-100" placeholder="YY/MM/DD" id="datePicker" />
-                      <i className="fa-regular fa-calendar-days fa-xl px-calender-icon" />
+            {/* start create customer invoice form */}
+            <form action className="d-flex flex-wrap">
+              <div className="side w-xxl-100 w-100 p-4">
+                <div className="section">
+                  <h4>{subTitle}</h4>
+                  <div className="form-inputs d-flex w-100">
+                    <div className="input-package mt-3 pe-2 d-flex flex-column w-50">
+                      <label className="mb-2" htmlFor={htmlFor}>
+                        {labelName}
+                      </label>
+                      <select
+                        name="customer"
+                        id="customer"
+                        className="px-login-input w-100 m-auto"
+                      >
+                        <option value>{optionName}</option>
+                        <option value="ahmed">ahmed</option>
+                        <option value="johan">johan</option>
+                        <option value="samy">samy</option>
+                      </select>
+                    </div>
+                    <div className="input-package mt-3 ps-2 d-flex flex-column w-50">
+                      <label className="mb-2" htmlFor="cost-center">
+                        cost center
+                      </label>
+                      <select
+                        name="cost-center"
+                        id="cost-center"
+                        className="px-login-input w-100 m-auto"
+                      >
+                        <option value>General cost center</option>
+                      </select>
                     </div>
                   </div>
-                  </div>
-              
-                  <div className='d-flex'>
-                  <div className="input-package mt-3 pe-2 d-flex flex-column w-50">
-                    <label className="mb-2" htmlFor="customer">currency</label>
-                    <select name="currency" id="currency" className="px-login-input  w-100 m-auto">
-                      <option value>select currency</option>
-                      <option value="dollar">dollar</option>
-                      <option value="euro">euro</option>
-                      <option value="dinar">dinar</option>
-                    </select>
-            
-                  </div>
-                  <div className="input-package mt-4 ps-2 d-flex flex-column w-50 ">
-                    <label className htmlFor>tax</label>
-                    <div className="px-calender w-100">
-                      <input type="text" className="px-form-input w-100" placeholder="Enter tax" />
-                    </div>
-                  </div>
-                  </div>
-              </div>
-              <div className="separetor my-4" />
-              <div className="section">
-                <h4>Cost Details</h4>
-                <div className="px-dashed-card d-flex justify-content-between p-3">
-                  <div className="total-text">
-                    <h5>total invoice</h5>
-                  </div>
-                  <div className="total-number">
-                    <h5>2150 $</h5>
+                  <div className="input-package mt-3 d-flex flex-column w-100">
+                    <label htmlFor="companyIndustry">Supplier Address</label>
+                    <textarea
+                      name="address"
+                      id="address"
+                      className="px-text-area"
+                      placeholder="Enter supplier address"
+                      defaultValue={""}
+                    />
                   </div>
                 </div>
+                <div className="separetor my-4" />
+                <div className="section">
+                  <div className="d-flex mt-4">
+                    <h4>Supply Items Details</h4>
+                    <button
+                      type="button"
+                      className="px-btn px-blue-btn add-item-btn ms-auto"
+                      onClick={handleAddItem}
+                    >
+                      Add Item
+                    </button>
+                  </div>
+
+                  {items.map((item, index) => (
+                    <div key={item.id} className="form-inputs w-100 d-flex justify-content-between flex-wrap item-details-container">
+                      <div className="input-package mt-4 d-flex flex-column w-30">
+                        <label>Item</label>
+                        <div className="px-calender mt-2 w-100">
+                          <select name="item" id="item" className="px-login-input w-100">
+                            <option value="item1">item1</option>
+                            <option value="item2">item2</option>
+                            <option value="item3">item3</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="input-package mt-4 d-flex flex-column w-30">
+                        <label>Price</label>
+                        <div className="px-calender mt-2 w-100">
+                          <select name="item-price" id="item-price" className="px-login-input w-100">
+                            <option value="100$">100$</option>
+                            <option value="200$">200$</option>
+                            <option value="300$">300$</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="input-package mt-4 d-flex flex-column w-30">
+                        <label>Item Count</label>
+                        <div className="px-calender w-100">
+                          <input type="text" className="px-form-input mt-2 w-100" placeholder={8} />
+                        </div>
+                      </div>
+                      <button
+                        type="button"
+                        className="px-btn px-red-btn remove-item-btn ms-auto"
+                        onClick={() => handleRemoveItem(item.id)}
+                      >
+                        -
+                      </button>
+                    </div>
+                  ))}
+
+                  {items.length > 0 && (
+                    <div className="px-dashed-card total-items-cost justify-content-between p-4 mt-4">
+                      <div className="total-text">
+                        <h5>Total Invoice</h5>
+                      </div>
+                      <div className="total-number">
+                        <h5>2150 $</h5>
+                      </div>
+                    </div>
+                  )}
+                </div>
+
+                <div className="separetor my-4" />
+                <div className="section">
+                  <h4>Invoice Details</h4>
+                  <div className="form-inputs d-flex flex-wrap w-100">
+                    <div className="input-package mt-4 pe-2 d-flex flex-column w-50">
+                      <label>Invoice due date</label>
+                      <div className="px-calender w-100">
+                        <input
+                          className="px-form-input w-100"
+                          placeholder="YY/MM/DD"
+                          id="datePicker"
+                        />
+                        <i className="fa-regular fa-calendar-days fa-xl px-calender-icon" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="d-flex">
+                    <div className="input-package mt-3 pe-2 d-flex flex-column w-50">
+                      <label className="mb-2" htmlFor="customer">
+                        Currency
+                      </label>
+                      <select
+                        name="currency"
+                        id="currency"
+                        className="px-login-input w-100 m-auto"
+                      >
+                        <option value>Select currency</option>
+                        <option value="dollar">Dollar</option>
+                        <option value="euro">Euro</option>
+                        <option value="dinar">Dinar</option>
+                      </select>
+                    </div>
+                    <div className="input-package mt-4 ps-2 d-flex flex-column w-50">
+                      <label>Tax</label>
+                      <div className="px-calender w-100">
+                        <input
+                          type="text"
+                          className="px-form-input w-100"
+                          placeholder="Enter tax"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="separetor my-4" />
+                <div className="section">
+                  <h4>Cost Details</h4>
+                  <div className="px-dashed-card d-flex justify-content-between p-3">
+                    <div className="total-text">
+                      <h5>Total Invoice</h5>
+                    </div>
+                    <div className="total-number">
+                      <h5>2150 $</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-action d-flex justify-content-end mt-5">
+                  <button className="px-btn px-white-btn">Cancel</button>
+                  <button className="px-btn px-blue-btn ms-4">Save</button>
+                </div>
               </div>
-              <div className="form-action d-flex justify-content-end mt-5">
-                <button className="px-btn px-white-btn">cancel</button>
-                <button className="px-btn px-blue-btn ms-4">save</button>
-              </div>
-            </div></form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-
-    
-    
-    </>
   )
 }
