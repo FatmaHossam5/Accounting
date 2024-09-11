c3f60ddeef532337150ef240ab07da3c6c7f5b9e
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2024-09-11 17:50:55 +0300

    getAllCountries

--- src/Components/Controls/Countries/Countries.jsx
+++ src/Components/Controls/Countries/Countries.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CustomPage from '../../Shared/CustomPage/CustomPage'
 import Modal from '../../Shared/Modal/Modal'
 import Input from '../../Shared/Input/Input'
 import Dropdown from '../../Shared/Dropdown/Dropdown'
 import Select from '../../Shared/Select/Select'
-
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 export default function Countries() {
     const [openDropdownId, setopenDropdownId] = useState(null)
+    const[country,setCountry]=useState([]);
+     const{register,handleSubmit,formState:{errors},reset } =useForm()
+    
     const columns = [
         {
             name: " Id",
-            selector: (row) => { row.id },
+            selector: (country) =>  country.id ,
             sortable: true,
             cell: (row) => (
                 <div className="d-flex align-items-center ">
@@ -46,87 +50,84 @@ export default function Countries() {
 
         },
         {
-            name: "Name",
-            selector: (row) => row.name,
+            name: "Arabic Name",
+            selector: (country) => country.countryAr?.name,
             sortable: true,
         },
         {
-            name: "Code",
-            selector: (row) => row.code,
+            name: "English Name",
+            selector: (country) => country?.countryEn?.name,
             sortable: true,
         },
-        {
-            name: "Slug",
-            selector: (row) => row.slug,
-            sortable: true,
-        },
-        {
-            name: "Currency",
-            selector: (row) => row.currency,
-            sortable: true,
-        },
+
 
 
     ];
 
-    const data = [
-        {
-            id: 1,
-            name: 'EGP',
-            code: "20",
-            slug: 'eg',
-            currency: 1,
-        },
+ 
+   
 
-        {
-            id: 2,
-            name: 'EGP',
-            code: "20",
-            slug: 'eg',
-            currency: 1,
-        },
-        {
-            id: 3,
-            name: 'USD',
-            code: "96",
-            slug: 'US',
-            currency: 2,
-        },
 
-    ];
+const getAllCountries=()=>{
+    axios.get(`http://192.168.1.7:8000/api/countries`).then((response)=>{
+        console.log(response.data.data);
+        setCountry(response.data.data)        
+    }).catch((error)=>{
+        console.log(error);
+        
 
-    const handleClose = () => alert('close')
+    })
+}
 
-    const handleSave = () => alert('save')
+const AddCountry=(data)=>{
+   
+    const formData = new FormData();
+    formData.append('name_ar', data.name_ar);
+    formData.append('name_en', data.name_en);
+    console.log(formData);
+    axios.post(`http://192.168.1.7:8000/api/countries`,formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    }).then((response)=>{
+        console.log(response);
+        getAllCountries()
+    }).catch((error)=>{
+        console.log(error);
+        
+    })
+}
+
+useEffect(()=>{
+    getAllCountries();
+},[])
     return (
    <>
                 <CustomPage
-                    title='currencies'
-                    ButtonName='Createcurrencies'
-                    ModalTitle='Create currencies'
-                    target='#createcurrencies'
+                    title='countries'
+                    ButtonName='Create Countries'
+                    ModalTitle='Create countries'
+                    target='#createcountries'
                     columns={columns}
-                    data={data}
+                    data={country}
                 />
-                <Modal id='createcurrencies' title='currencies' onSave={handleSave} onCancel={handleClose} className='w-40'>
-                    <form action>
+                <Modal id='createcountries' title='countries'  className='w-40'onSave={handleSubmit(AddCountry)}>
+                    <form  onSubmit={handleSubmit(AddCountry)}>
+               
                         <div className="  ">
-                            <div className="">
+                            <div className="col-12 d-flex">
 
-                                <div className="input-package my-3 pe-2 d-flex flex-column w-70 m-auto">
-                                    <Input label=' Name' placeholder=' Name ' className="px-form-input w-100 m-auto" />
+                                <div className="input-package my-3 pe-2 d-flex flex-column col-6">
+                                    <Input label='Arabic Country Name' placeholder='Enter Arabic Country Name ' className="px-form-input w-100 m-auto"  {...register('name_ar', { required: "Arabic name is required" })} />
+                              
                                 </div>
-                                <div className="input-package my-3 pe-2 d-flex flex-column w-70 m-auto">
-                                    <Input label='Commercial Register' placeholder='Commercial Register ' className="px-form-input w-100 m-auto" />
+                                <div className="input-package my-3 pe-2 d-flex flex-column col-6">
+                                    <Input label='English Country Name' placeholder='Enter English Country Name ' className="px-form-input w-100 m-auto"  {...register('name_en', { required: "English name is required" })}/>
                                 </div>
-                                <div className="input-package my-3 pe-2 d-flex flex-column w-70 m-auto">
-                                    <Input label='Logo' placeholder='Logo ' className="px-form-input w-100 m-auto" />
-                                </div>
+                             
                             </div>
                         </div>
-                        <div className="modal-footer w-100">
-                            <button type="button" className="px-btn btn px-white-btn" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" className="px-btn px-blue-btn">save</button>
+                        <div className="modal-footer  mt-3 ms-5">
+                            <button type="button" className="px-btn btn px-white-btn " data-bs-dismiss="modal">Cancel</button>
+                            <button type="submit" className="px-btn px-blue-btn ">save</button>
                         </div>
                     </form>
                 </Modal>
